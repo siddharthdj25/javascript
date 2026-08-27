5d9fc25934b2896a0e7ad0e249810c8c6cfec13d
siddharthdj25 <siddharthdj25@gmail.com>
2026-08-27 19:22:10 +0530

    live watch

--- timeinterval.js
+++ timeinterval.js
@@ -1,8 +1,17 @@
-function delayedFunction (){
-    console.log("this function is delayed by the 2 seconds ");
-}
-setTimeout(delayedFunction,2000);
-function setinterval(){
+// function delayedFunction (){
+//     console.log("this function is delayed by the 2 seconds ");
+// }
+// setTimeout(delayedFunction,2000);
+// function setinterval(){
+//     console.log("this function is called after every 2 seconds ");
+// }
+// setInterval(setinterval,2000);
+const repeatedFunction = ()=>{
     console.log("this function is called after every 2 seconds ");
+};
+const intervalid=setInterval(repeatedFunction,1000);
+function stopInterval() {
+    clearInterval(intervalid);
 }
-setInterval(setinterval,2000);
+
+setTimeout(stopInterval, 5000);
