5acd2047359eb285ad58e64d0d6632def88d0811
siddharthdj25 <siddharthdj25@gmail.com>
2026-09-01 15:36:49 +0530

    objectjs

---
+++ object.js
@@ -0,0 +1,70 @@
+let person={
+    name: "siddharth",
+    age: 20,
+    "isStudent":false,
+    greet: function (){
+        console.log("welcome baby")
+    },
+
+};
+person.job="web dev";
+person[`age`]=18;
+console.log(person.age);
+console.log(person.name);
+console.log(person[`isStudent`]);
+console.log(person);
+console.log(person.greet());
+idType="studentId";
+let student={
+    [idType]:"A1234567",
+    sname:"siddharth",
+    sage:20,
+    isstudent:true,
+    greet:function(){
+        console.log(
+            `hey my ${idType} is ${student[idType]} and my name is ${student.sname}`
+        );
+       
+    },
+};
+student.greet(); 
+
+let obj={ id:5,name:"kodyfier"};
+let obj1=obj;
+obj1.name="siddharth";
+console.log(obj1);
+console.log("original obj",obj);
+
+
+let obj2={ id:5,name:"kodyfier"};
+let newobj=Object.assign({},obj2);
+newobj.name="siddharth";
+console.log(newobj);
+console.log("original obj2",obj2);
+
+
+const obj5={name:"sidd"};
+const obj6={name:"sidd"};
+
+const isEqual=obj5===obj6 ? true:false;
+console.log(isEqual);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,17 @@
-const date =new Date("2024-01-05");
-console.log(date);
+idType="studentId";
+let student={
+    [idType]:"A1234567",
+    sname:"siddharth",
+    sage:20,
+    isstudent:true,
+    greet:function(){
+        console.log(
+            `hey my ${idType} is ${student[idType]} and my name is ${student.sname}`
+        );
+        studemt.greet();
+    },
+
+
+
+};
+   student.greet();
